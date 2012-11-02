91a354c6f98afc70749a8d251f7e7c7fafbc1f11
Marcus Ortiz <mportiz08@gmail.com>
2012-11-02 14:42:15 -0700

    Removed some unecessary stuff.

--- qlmd/GeneratePreviewForURL.m
+++ qlmd/GeneratePreviewForURL.m
@@ -13,46 +13,8 @@
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 
-/* -----------------------------------------------------------------------------
-   Generate a preview for file
-
-   This function's job is to create preview for designated file
-   ----------------------------------------------------------------------------- */
-
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
-//  CFDataRef data;
-//  CFStringRef html;
-//  struct html_renderopt opts;
-//  struct sd_callbacks callbacks;
-//  struct sd_markdown *markdown;
-//  SInt32 error;
-//  CFRange range;
-//  UInt8 *bytes;
-//  struct buf *in_buff, *out_buff;
-//  
-//  CFURLCreateDataAndPropertiesFromResource(NULL, url, &data, NULL, NULL, &error);
-//  range = CFRangeMake(0, CFDataGetLength(data));
-//  bytes = (UInt8 *)malloc(range.length * sizeof(UInt8));
-//  CFDataGetBytes(data, CFRangeMake(0, CFDataGetLength(data)), bytes);
-//  
-//  in_buff = bufnew(range.length);
-//  bufput(in_buff, bytes, range.length);
-//  out_buff = bufnew(OUTPUT_UNIT);
-//  
-//  sdhtml_renderer(&callbacks, &opts, 0);
-//  markdown = sd_markdown_new(EXTENSIONS, MAX_NESTING, &callbacks, &opts);
-//  sd_markdown_render(out_buff, in_buff->data, in_buff->size, markdown);
-//
-//  html = CFStringCreateWithBytes(NULL, out_buff->data, out_buff->size, kCFStringEncodingUTF8, true);
-//  data = CFStringCreateExternalRepresentation(NULL, html, kCFStringEncodingUTF8, 0);
-//  QLPreviewRequestSetDataRepresentation(preview, data, kUTTypeHTML, NULL);
-//  
-//  sd_markdown_free(markdown);
-//  bufrelease(out_buff);
-//  bufrelease(in_buff);
-//  free(bytes);
-//  CFRelease(data);
   NSMutableDictionary *props;
   NSMutableString *html;
   NSMutableString *rendered;
@@ -95,6 +57,10 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
     QLPreviewRequestSetDataRepresentation(preview,
       (CFDataRef)CFBridgingRetain([html dataUsingEncoding:NSUTF8StringEncoding]),
       kUTTypeHTML, (CFDictionaryRef)CFBridgingRetain(props));
+    
+    sd_markdown_free(markdown);
+    bufrelease(outbuf);
+    bufrelease(inbuf);
   }
   
   return noErr;
@@ -102,5 +68,5 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
 
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview)
 {
-  // Implement only if supported
+  // Not supported
 }
